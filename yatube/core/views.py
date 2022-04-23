1d8ae861fecb2da8d3758cf1492b2cf95dd12b2f
Алымова Мария <alymova.backend@gmail.com>
2022-04-23 08:41:36 +0300

    Доработан шаблонz главной страницы

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -7,3 +7,6 @@ def page_not_found(request, exception):
 
 def permission_denied(request, exception):
     return render(request, 'core/403.html', status=403)
+
+def server_error(request):
+    return render(request, 'core/500.html', status=500)
