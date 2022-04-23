b44a8f91332dd29f3798f8e51bfbf4f906115f1e
Mary <vyzeika@gmail.com>
2022-04-23 12:21:23 +0300

    Доработаны views для доступа к списку подписчиков

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -145,8 +145,7 @@ def follow_index(request):
 def profile_follow(request, username):
     user = request.user
     author = get_object_or_404(User, username=username)
-    if user != author and not now_follower:
-        Follow.objects.get_or_create(user=user, author=author)
+    Follow.objects.get_or_create(user=user, author=author)
     return redirect('posts:profile', username=username)
 
 
